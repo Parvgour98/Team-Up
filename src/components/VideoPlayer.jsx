3c47df218bf1efeeda6f8542ea01c297693b36a9
Parvgour98 <46477131+Parvgour98@users.noreply.github.com>
2021-07-05 12:45:00 +0530

    Update VideoPlayer.jsx

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -5,7 +5,7 @@ import { SocketContext } from '../Context';
 
 const useStyles = makeStyles((theme) => ({
   video: {
-    width: '500px',
+    width: '400px',
     [theme.breakpoints.down('xs')]: {
       width: '300px',
     },
